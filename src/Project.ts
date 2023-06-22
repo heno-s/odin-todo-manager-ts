49d985d94e3a2e53c4f0911d7846ef0ffeffc1aa
janko12 <henrich.sury97@gmail.com>
2023-06-22 20:15:05 +0200

    Pass tests for Project module

--- src/Project.ts
+++ src/Project.ts
@@ -3,11 +3,21 @@ import Task from "./Task";
 export default class Project {
     id: string = uid();
     tasks: Task[] = [];
-    addTask(task: Task): void {}
-    deleteTask(id: string): boolean {
-        return false;
+    addTask(task: Task): void {
+        this.tasks.push(task);
     }
     getTask(id: string): Task | null {
-        return null;
+        return this.tasks.find((task) => task.id === id) || null;
+    }
+    deleteTask(id: string): boolean {
+        const taskIndex = this.tasks.findIndex(
+            (task) => task.id === id
+        );
+        if (taskIndex !== -1) {
+            this.tasks.splice(taskIndex, 1);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
