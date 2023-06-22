74a4debf0c2b294e4c3df02a0d8e4e8bc9721cf0
janko12 <henrich.sury97@gmail.com>
2023-06-22 20:28:47 +0200

    Write tests for ProjectList module

---
+++ src/__tests__/ProjectList.test.ts
@@ -0,0 +1,46 @@
+import ProjectList from "../ProjectList";
+import Project from "../Project";
+
+it("should add Project to projects array", () => {
+    const projectList = new ProjectList();
+    const project = new Project();
+    projectList.addProject(project);
+    expect(projectList.projects.length).toBe(1);
+});
+
+it("should get project from project array specified by id", () => {
+    const projectList = new ProjectList();
+    const project = new Project();
+    projectList.addProject(project);
+    const retrievedProject = project.getTask(project.id);
+    expect(retrievedProject).toBe(project);
+});
+
+it("should get correct project from project array specified by id", () => {
+    const projectList = new ProjectList();
+    const project = new Project();
+    const project2 = new Project();
+    projectList.addProject(project);
+    projectList.addProject(project2);
+    const retrievedProject = projectList.getProject(project2.id);
+    expect(retrievedProject).not.toBe(project);
+    expect(retrievedProject).toBe(project2);
+});
+
+it("should delete project from project array", () => {
+    const projectList = new ProjectList();
+    const project = new Project();
+    projectList.addProject(project);
+    projectList.deleteProject(project.id);
+    expect(projectList.projects.length).toBe(0);
+});
+
+it("should delete the correct project from project array", () => {
+    const projectList = new ProjectList();
+    const project = new Project();
+    const project2 = new Project();
+    projectList.addProject(project);
+    projectList.addProject(project2);
+    projectList.deleteProject(project.id);
+    expect(projectList.projects[0].id).toBe(project2.id);
+});
